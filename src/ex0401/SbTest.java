ed6a0110d0f4d91ac656d81557507ce65bd5c822
Mackeabit <thdrudgh7775@naver.com>
2023-04-01 23:44:41 +0900

    - 쓰레드 테스트 코드

---
+++ src/ex0401/SbTest.java
@@ -0,0 +1,40 @@
+package ex0401;
+
+public class SbTest implements Runnable {
+    StringBuilder sb;
+
+    public SbTest() {
+        sb = new StringBuilder();
+    }
+
+    public void run() {
+        addChar();
+    }
+
+    public void addChar() {
+        for (int i = 0; i < 10000; i++) {
+            sb.append("1");
+            sb.append("0");
+        }
+    }
+
+    public static void main(String[]args){
+        SbTest sbTest = new SbTest();
+        Thread threadOne = new Thread(sbTest, "ThreadOne");
+        Thread threadTwo = new Thread(sbTest, "ThreadTwo");
+
+        threadOne.start();
+        threadTwo.start();
+
+        try {
+            threadOne.join();
+            threadTwo.join();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("length:" + sbTest.sb.length());
+
+    }
+}
